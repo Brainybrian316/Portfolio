d763305b2cbd9edc1383f5f4bea4153ba7a6fa46
Brainybrian316 <brainybrian316@gmail.com>
2022-07-01 16:32:37 -0400

    about styling

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -3,7 +3,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
-
+import Image from 'react-bootstrap/Image';
 
 export default function About() {
   return (
@@ -15,90 +15,86 @@ export default function About() {
             <hr />
           </Col>
         </Row>
-      
+      </Container>
 
-
-				<Row>
-          <Col md={6} sm={12} className="d-flex justify-content-center">
-            <Card style={{ width: '50rem' }} className="bg-black text-white ">
-              <Card.Img
-                variant="top"
-                src={require('../../assets/images/Prof-pic.png')}
-                alt="Brian"
-                className="rounded pb-3"
-              />
-              <h1 className='text-info'>Brian Mojica</h1>
-              <Card.Text>
-                <p>
-                  Mission-driven full-stack Web Developer & Educator with a passion for thoughtful
-                  UI, collaboration, and teaching. I am innovative problem-solver with a passion for developing apps. I am continuing my studies by expanding my knowledge in IOS development, more languages, libraries, and concepts. 
-                </p>
-              </Card.Text>
-              <ListGroup className="list-group-flush rp">
-                <ListGroup.Item className="bg-black text-white">
-                  Full-Stack Web Developer & Educator
-                </ListGroup.Item>
-                <ListGroup.Item className="bg-black text-white">
-                  University of Central Florida Graduate
-                </ListGroup.Item>
-                <ListGroup.Item className="bg-black text-white">
-                  <i className="fa-regular fa-bookmark"></i> HTML, CSS, JavaScript, & React.js{' '}
-                  <i className="fa-regular fa-heart"></i>
-                </ListGroup.Item>
-              </ListGroup>
-              <Card.Body>
-                <Card.Link href="https://github.com/Brainybrian316" className='fs-1'>
-                  <i className="fa-brands fa-github-alt px-2 text-warning"></i>
-                </Card.Link>
-                <Card.Link href="https://www.linkedin.com/in/brian-mojica-63b638221" className='fs-1'>
-                  <i className="fa-brands fa-linkedin px-2 text-warning"></i>
-                </Card.Link>
-                <Card.Link href="mailto:brainybrian316@gmail.com" className='fs-1'>
-                  <i className="fa-solid fa-envelope px-2 text-warning"></i>
-                </Card.Link>
-              </Card.Body>
-            </Card>
+      <Container className="bg-black text-white rounded">
+        <Row>
+          <Col lg={4} md={4} sm={12}>
+            <Image
+              src={require('../../assets/images/Prof-pic.png')}
+              alt="Brian"
+              className="rounded img-fluid"
+            />
+            <h1 className="text-info">Brian Mojica</h1>
           </Col>
-				
-
-
-          <Col md={6} sm={12} className="d-flex justify-content-center mt-2" >
-            <Card style={{ width: '50rem' }} className="bg-black text-white">
-              <Card.Body>
-								<h1 className='text-info'>Technical Skills:</h1>
-                <Card.Text>
-                  <p>
-                  MERN Stack (MongoDB, Express.js, React.js, Node.js) jQuery,
-                    Bootstrap, JavaScript, MySQL
-                  </p>
-                  <p>
-                    Core Concepts: Security and Session Storage, User Authentication, Responsive
-                    Design, Cookies, Local Storage
-                  </p>
-
-                  <p className="pt-3 mt-3">
-                  My goal and desire are to leave my mark in the tech world whether that be through open source contributions, project tutorials, and of course the career opportunities I endeavor in.
-                  </p>
-                  <p className="pt-3 mt-3">
-                    What I bring to every project:
-                    <br />
-                    <ul>
-                      <li> Problem-solving skills and perseverance</li>
-                      <li> An understanding of UX/UI principles</li>
-                      <li>Creativity and passion</li>
-                      <li>Dedication to teamwork and flexible collaboration</li>
-                      <li>Foundational knowledge of agile software development principles</li>
-                    </ul>
-                    I am excited to leverage my skills in the future as part of a quality-driven
-                    team. Let's <a href="#Contact">connect</a> and chat about our next project.
-                  </p>
-                </Card.Text>
-              </Card.Body>
-            </Card>
+          <Col lg={4} md={4} sm={12}>
+            <p>
+              Mission-driven full-stack Web Developer & Educator with a passion for thoughtful UI,
+              collaboration, and teaching. I am innovative problem-solver with a passion for
+              developing apps. I am continuing my studies by expanding my knowledge in IOS
+              development, more languages, libraries, and concepts.
+            </p>
           </Col>
-				</Row>
+          <Col lg={4} md={4} sm={12}>
+            <ListGroup className="list-group-flush rp">
+              <ListGroup.Item className="bg-black text-white">
+                Full-Stack Web Developer & Educator
+              </ListGroup.Item>
+              <ListGroup.Item className="bg-black text-white">
+                University of Central Florida Graduate
+              </ListGroup.Item>
+              <ListGroup.Item className="bg-black text-white">
+                <i className="fa-regular fa-bookmark"></i> HTML, CSS, JavaScript, & React.js{' '}
+                <i className="fa-regular fa-heart"></i>
+              </ListGroup.Item>
+            </ListGroup>
+            <a href="https://github.com/Brainybrian316" className="fs-1">
+              <i className="fa-brands fa-github-alt px-2 text-warning"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/brian-mojica-63b638221" className="fs-1">
+              <i className="fa-brands fa-linkedin px-2 text-warning"></i>
+            </a>
+            <a href="mailto:brainybrian316@gmail.com" className="fs-1">
+              <i className="fa-solid fa-envelope px-2 text-warning"></i>
+            </a>
+          </Col>
+        </Row>
+      </Container>
 
-			</Container>
+      <Container className="bg-black text-white rounded mt-2">
+        <Row>
+          <Col md={6} sm={12}>
+            <h1 className="text-info">Technical Skills:</h1>
+            <p>
+              MERN Stack (MongoDB, Express.js, React.js, Node.js) jQuery, Bootstrap, JavaScript,
+              MySQL
+            </p>
+            <p>
+              Core Concepts: Security and Session Storage, User Authentication, Responsive Design,
+              Cookies, Local Storage
+            </p>
+            <p>
+              My goal and desire are to leave my mark in the tech world whether that be through open
+              source contributions, project tutorials, and of course the career opportunities I
+              endeavor in.
+            </p>
+          </Col>
+          <Col md={6} sm={12}>
+            <p>
+              What I bring to every project:
+              <ul>
+                <li> Problem-solving skills and perseverance</li>
+                <li> An understanding of UX/UI principles</li>
+                <li>Creativity and passion</li>
+                <li>Dedication to teamwork and flexible collaboration</li>
+                <li>Foundational knowledge of agile software development principles</li>
+              </ul>
+              I am excited to leverage my skills in the future as part of a quality-driven team.
+              Let's <a href="#Contact">connect</a> and chat about our next project.
+            </p>
+          </Col>
+        </Row>
+      </Container>
     </section>
   );
 }
