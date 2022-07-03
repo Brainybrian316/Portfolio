55f8c7f7f6dba6bc8c4795ce5dc7bc8c6eab7252
Brainybrian316 <brainybrian316@gmail.com>
2022-07-02 22:16:17 -0400

    animation with proper overlay

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,38 +1,48 @@
-import { Button, Col, Image, Overlay, OverlayTrigger, } from 'react-bootstrap';
+import { Button, Card, Col, Image, Overlay, OverlayTrigger } from 'react-bootstrap';
 import GalleryModal from '../Modal';
 import React, { useState } from 'react';
 import './gallery.css';
-
+import 'animate.css';
 
 export default function ProjectCard({ project }) {
   const [modalShow, setModalShow] = useState(false);
-  const [show, setShow] = useState(false);
-
 
   return (
     <>
-          <Col lg={4} md={6} sm={12}>
-            <div className="hovereffect">
-              <Image src={project.image} fluid rounded  ></Image>
-              <div className="overlay">
-                <h2>
-                  {project.title}
-                  <br />
-                  <span className="languages">{project.languages}</span>
-                </h2>
-                <Button className="info"  onClick={() => setModalShow(true)}>
-                  Learn More
-                </Button>
-              </div>
-            </div>
-          </Col>
+      <Col lg={4} md={6} sm={12}>
+        <div class="hovereffect">
+          <Image src={project.image} alt={project.title} fluid className="image" />
+          <div className="overlay">
+            <h2 className="animated">
+              {project.title}
+              <br />
+              <span className="languages">{project.languages}</span>
+            </h2>
+            <Button className="info animated"  onClick={() => setModalShow(true)}>
+    Learn More
+  </Button>
+          </div>
+        </div>
+      </Col>
 
-                {/* after modal close set to show projectCard */}
-          <GalleryModal
-            show={modalShow}
-            onHide={() => setModalShow(false)}
-            project={project}
-          />
+      <GalleryModal show={modalShow} onHide={() => setModalShow(false)} project={project} />
     </>
   );
+}
+
+{
+  /* <div className="hovereffect">
+<Image id='img' src={project.image} fluid rounded ></Image>
+<div className="overlay">
+  <h2>
+    {project.title}
+    <br />
+    <span className="languages">{project.languages}</span>
+  </h2>
+  <Button className="info"  onClick={() => setModalShow(true)}>
+    Learn More
+  </Button>
+
+</div>
+</div> */
 }
