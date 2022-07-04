485df155d0ab528073b7aa3a69dec691438983b0
Brainybrian316 <brainybrian316@gmail.com>
2022-07-04 11:09:11 -0400

    functional contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,35 +1,98 @@
+import { useState, useRef } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import emailjs from '@emailjs/browser';
 
-export default function Contact() {
+const Result = () => {
+	return(
+		<p>Your message has been successfully sent. I will contact you soon.</p>
+	)
+}
+
+export default function Contact(props) {
+
+
+	const [result, showResult] = useState(false);
+
+	
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('portfolio-contact', 'template_meymdpm', e.target, '9D0f7rikY489nEQZS')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+			e.target.reset();
+			showResult(true);
+  };
+
+	setTimeout(() => {
+		showResult(false); },
+		 5000);
+
   return (
     <div>
+      <Container>
+        <Row>
+          <Col className="mt-5 pt-5">
+            <h1 className="text-center">Contact</h1>
+            <hr />
+          </Col>
+        </Row>
+      </Container>
 
-      <div class="col-12 mt-5 pt-5">
-			<div className="text-center text-dark">
-				<h1 id="Contact">Contact</h1>
-			</div>
-		</div>
+      <div className="container mb-5">
+        <div className="row d-flex justify-content-center text-white">
+          <div className="col-lg-6 col-md-12 col-sm-12">
+            <form className="contact-form"  action="" onSubmit={sendEmail}>
+              <label for="Name" className="form-label">
+                Name:
+              </label>
+              <input
+                type="text"
+                name="name"
+                className="form-control"
+                id="Name"
+                placeholder="Enter your name"
+                required
+              />
 
-    <div className="container mb-5">
-			<div className="row d-flex justify-content-center text-white">
-				<div className="col-lg-6 col-md-12 col-sm-12">
-					<form className="contact-form">
+              <label for="Email" className="form-label">
+                Email:
+              </label>
+              <input
+                type="email"
+                name="email"
+                className="form-control"
+                id="Email"
+                placeholder="Enter your email"
+                required
+              />
 
-						<label for="Name" className="form-label">Name:</label>
-						<input type="text" name="Name" className="form-control" id="Name" placeholder="Enter your name" required/>
+              <label for="Message" className="form-label">
+                Message:
+              </label>
+              <textarea
+                name="message"
+                className="form-control"
+                id="Message"
+                rows="3"
+                placeholder="Enter your message"
+                required
+              ></textarea>
 
-						<label for="Email" className="form-label">Email:</label>
-						<input type="email" name="Email" className="form-control" id="Email" placeholder="Enter your email" required/>
-
-						<label for="Message" className="form-label">Message:</label>
-						<textarea name="Message" className="form-control" id="Message" rows="3" placeholder="Enter your message" required></textarea>
-
-						<button type="submit" className="btn btn-primary mt-3">Submit</button>
-
-					</form>
-				</div>
-			</div>
-		</div>
-
+              <button type="submit" className="btn btn-primary mt-3" value="Send">
+                Submit
+              </button>
+							<div>{result ? <Result/> : null}</div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
+
+// process.env.REACT_APP_YOUR_SERVICE_ID, process.env.REACT_APP_YOUR_TEMPLATE_ID, form.current, process.env.REACT_APP_YOUR_PUBLIC_KEY
+
