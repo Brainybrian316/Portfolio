e89df4b22b2fc4005d55b99a3fab21c49c7ed0c1
Brainybrian316 <brainybrian316@gmail.com>
2022-07-05 13:49:08 -0400

    blog links work

--- src/utils/dbBlogs.js
+++ src/utils/dbBlogs.js
@@ -5,20 +5,20 @@ export const dbBlogsList =  [
     image: require('../assets/images/blog1.png'),
     title: "All About Me",
     description: "I have a passion for creating beautiful, functional, and intuitive user experiences. I'm currently looking for an entry level position.",
-    link: "/"
+    link: "https://medium.com/@brainybrian316/under-construction-75c2ff2c33f5"
   },
   {
     id: 2,
     image: require('../assets/images/blog2.png'),
     title: "My boot camp experience",
     description: "I enjoyed my boot camp experience.",
-    link: "/"
+    link: "https://medium.com/@brainybrian316/under-construction-75c2ff2c33f5"
   },
   {
     id: 3,
     image: require('../assets/images/blog3.png'),
     title: "Why I left teaching",
     description: "I can write a book on this stuff.",
-    link: "/"
+    link: "https://medium.com/@brainybrian316/under-construction-75c2ff2c33f5"
   }
 ]
