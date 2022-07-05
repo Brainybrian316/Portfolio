7c0f06734a2e40f6949ff300765db62f3741ac47
Brainybrian316 <brainybrian316@gmail.com>
2022-07-05 15:01:23 -0400

    fade animation

--- src/App.js
+++ src/App.js
@@ -8,24 +8,44 @@ import Footer from './components/Footer';
 import '../src/assets/css/style.css';
 import Container from 'react-bootstrap/Container';
 import Hero from './components/Hero';
+import Aos from 'aos';
+import 'aos/dist/aos.css'; 
+import { useEffect } from 'react';
+
 
 
 function App() {
+  useEffect(() => {
+  Aos.init(
+    { duration: 1000, once: true });
+}, []);
 
   return (
     <body>
       <Navbar />
       <Hero/>
-      <About/>
-      <FeaturedProjects/>
+      <div 
+      data-aos="fade-up"
+      data-aos-offset="360"
+      >
+      <About />
+      </div>
+      <div data-aos="fade-right">
+      <FeaturedProjects />
+      </div>
+      <div data-aos="fade-right">
       <Projects />
+      </div>
+      <div data-aos="fade-right">
       <Blog/>
+      </div>
 
-      
+      <div data-aos="fade-up">
       <Container id="curve" fluid >
       <Contact/>
       <Footer/>
       </Container>
+      </div>
 
     </body>
   );
