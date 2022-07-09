c62f49bb538a7aca2396e96e23c0d3fa5c1640fa
Brainybrian316 <brainybrian316@gmail.com>
2022-07-09 18:39:40 -0400

    cloudinary blog images

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -3,33 +3,31 @@ import { Container, Row, Col, Image, Carousel } from 'react-bootstrap';
 import './slider.css';
 
 export default function Blog() {
-  const blog = dbBlogsList;
 
-  // set blog.primarySource to fallback on blog.image if it is not available
-  blog.forEach((blog) => {
+
+  dbBlogsList.forEach((blog) => {
     if (!blog.primarySource) {
       blog.primarySource = blog.image;
     }
   });
+
 
   return (
     <>
       <Container>
         <Row>
           <Col className="mt-5 pt-5">
-            <h1 className="text-center" id="Blog">
-              Blog
-            </h1>
+            <h1 className="text-center" id="Blog">Blog</h1>
             <hr />
           </Col>
         </Row>
       </Container>
       <Container>
-        <Carousel>
-          {blog.map((blog) => (
-            <Carousel.Item key={blog.id} interval={10000}>
-              <Image src={blog.primarySource} alt={blog.title} fluid />
-              <Carousel.Caption>
+        <Carousel fade>
+          {dbBlogsList.map((blog, index) => (
+            <Carousel.Item key={index} interval={10000}>
+              <Image src={blog.primarySource} alt={blog.title} rounded fluid/>
+              <Carousel.Caption >
                 <h3>{blog.title}</h3>
                 <p>{blog.description}</p>
                 <a
